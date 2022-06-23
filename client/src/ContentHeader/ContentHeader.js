557976357dae3732cd860a1a37cdc2a234cb0efa
likagogishvili <likagogishvili2@gmail.com>
2022-06-23 09:54:48 +0400

    fixing header media

--- client/src/ContentHeader/ContentHeader.js
+++ client/src/ContentHeader/ContentHeader.js
@@ -19,53 +19,58 @@ function ContentHeader() {
     >
       <div className="divHeaderCont">
         <div className="headerContentSmall">
-          <a href="https://www.geostat.ge/ka" target="_blank" rel="noreferrer" className="divHeaderCont1">
+          <a
+            href="https://www.geostat.ge/ka"
+            target="_blank"
+            rel="noreferrer"
+            className="divHeaderCont1"
+          >
             <img src={headerLogo} alt="" width={"50%"} height={"100%"} />
           </a>
-        <div className="divHeaderCont2">
-          <p>დარგების ეკონომიკური ანალიზი</p>
+          <div className="divHeaderCont2">
+            <p>დარგების ეკონომიკური ანალიზი</p>
+          </div>
+          <div className="imgDiv">
+            <a
+              href="https://www.facebook.com/geostat.ge/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={facebook} alt="" width={"100%"} height={"100%"} />
+            </a>
+            <a
+              href="https://twitter.com/Geostat100"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={twitter} alt="" width={"100%"} height={"100%"} />
+            </a>
+            <a
+              href="https://www.youtube.com/user/CensusGe"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={youtube} alt="" width={"100%"} height={"100%"} />
+            </a>
+            {!languageChange ? (
+              <img
+                src={imgLink[0]}
+                alt=""
+                width={"100%"}
+                height={"100%"}
+                onClick={() => SetLanguageChange(!languageChange)}
+              />
+            ) : (
+              <img
+                src={imgLink[1]}
+                alt=""
+                width={"100%"}
+                height={"100%"}
+                onClick={() => SetLanguageChange(!languageChange)}
+              />
+            )}
+          </div>
         </div>
-        <div className="imgDiv">
-          <a
-            href="https://www.facebook.com/geostat.ge/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={facebook} alt="" width={"100%"} height={"100%"} />
-          </a>
-          <a
-            href="https://twitter.com/Geostat100"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={twitter} alt="" width={"100%"} height={"100%"} />
-          </a>
-          <a
-            href="https://www.youtube.com/user/CensusGe"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={youtube} alt="" width={"100%"} height={"100%"} />
-          </a>
-          {!languageChange ? (
-            <img
-              src={imgLink[0]}
-              alt=""
-              width={"100%"}
-              height={"100%"}
-              onClick={() => SetLanguageChange(!languageChange)}
-            />
-          ) : (
-            <img
-              src={imgLink[1]}
-              alt=""
-              width={"100%"}
-              height={"100%"}
-              onClick={() => SetLanguageChange(!languageChange)}
-            />
-          )}
-        </div>
-      </div>
       </div>
     </header>
   );
